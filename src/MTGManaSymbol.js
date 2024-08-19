ceccaa8ce63e67fddd25e8d9965e5d5be2c4b75d
Teddy Walsh <teddywalsh72@gmail.com>
2024-08-19 04:02:24 -0400

    Fixed incorrect filePath in MTGManaSymbol.js

--- src/MTGManaSymbol.js
+++ src/MTGManaSymbol.js
@@ -1,4 +1,5 @@
 // Symbol files from: https://mtg.fandom.com/wiki/Category:Mana_symbols
+let rootFolder = "../../media/mtg/";
 
 window.onload = () => {
     let paragraphs = document.querySelectorAll("*");
@@ -10,45 +11,45 @@ window.onload = () => {
         // Reaplce the placeholders with the mana symbols
 
         // Colored Manna
-        text = text.replaceAll("[w]", "<img src=\"/media/mtg/w.svg\" class=\"mannaSymbol\">");
-        text = text.replaceAll("[u]", "<img src=\"/media/mtg/u.svg\" class=\"mannaSymbol\">");
-        text = text.replaceAll("[b]", "<img src=\"/media/mtg/b.svg\" class=\"mannaSymbol\">");
-        text = text.replaceAll("[r]", "<img src=\"/media/mtg/r.svg\" class=\"mannaSymbol\">");
-        text = text.replaceAll("[g]", "<img src=\"/media/mtg/g.svg\" class=\"mannaSymbol\">");
+        text = text.replaceAll("[w]", `<img src=\"${rootFolder}w.svg\" class=\"mannaSymbol\">`);
+        text = text.replaceAll("[u]", `<img src=\"${rootFolder}u.svg\" class=\"mannaSymbol\">`);
+        text = text.replaceAll("[b]", `<img src=\"${rootFolder}b.svg\" class=\"mannaSymbol\">`);
+        text = text.replaceAll("[r]", `<img src=\"${rootFolder}r.svg\" class=\"mannaSymbol\">`);
+        text = text.replaceAll("[g]", `<img src=\"${rootFolder}g.svg\" class=\"mannaSymbol\">`);
 
         // Hybrid Manna ("(wu)" and "(uw)" should both work)
-        text = text.replaceAll("[wu]", "<img src=\"/media/mtg/WU.svg\" class=\"mannaSymbol\">");
-        text = text.replaceAll("[uw]", "<img src=\"/media/mtg/WU.svg\" class=\"mannaSymbol\">");
-        text = text.replaceAll("[wb]", "<img src=\"/media/mtg/WB.svg\" class=\"mannaSymbol\">");
-        text = text.replaceAll("[bw]", "<img src=\"/media/mtg/WB.svg\" class=\"mannaSymbol\">");
-        text = text.replaceAll("[wr]", "<img src=\"/media/mtg/RW.svg\" class=\"mannaSymbol\">");
-        text = text.replaceAll("[rw]", "<img src=\"/media/mtg/RW.svg\" class=\"mannaSymbol\">");
-        text = text.replaceAll("[wg]", "<img src=\"/media/mtg/GW.svg\" class=\"mannaSymbol\">");
-        text = text.replaceAll("[gw]", "<img src=\"/media/mtg/GW.svg\" class=\"mannaSymbol\">");
-        text = text.replaceAll("[ub]", "<img src=\"/media/mtg/UB.svg\" class=\"mannaSymbol\">");
-        text = text.replaceAll("[bu]", "<img src=\"/media/mtg/UB.svg\" class=\"mannaSymbol\">");
-        text = text.replaceAll("[ur]", "<img src=\"/media/mtg/UR.svg\" class=\"mannaSymbol\">");
-        text = text.replaceAll("[ru]", "<img src=\"/media/mtg/UR.svg\" class=\"mannaSymbol\">");
-        text = text.replaceAll("[ug]", "<img src=\"/media/mtg/GU.svg\" class=\"mannaSymbol\">");
-        text = text.replaceAll("[gu]", "<img src=\"/media/mtg/GU.svg\" class=\"mannaSymbol\">");
-        text = text.replaceAll("[br]", "<img src=\"/media/mtg/BR.svg\" class=\"mannaSymbol\">");
-        text = text.replaceAll("[rb]", "<img src=\"/media/mtg/BR.svg\" class=\"mannaSymbol\">");
-        text = text.replaceAll("[bg]", "<img src=\"/media/mtg/BG.svg\" class=\"mannaSymbol\">");
-        text = text.replaceAll("[gb]", "<img src=\"/media/mtg/BG.svg\" class=\"mannaSymbol\">");
-        text = text.replaceAll("[rg]", "<img src=\"/media/mtg/RG.svg\" class=\"mannaSymbol\">");
-        text = text.replaceAll("[gr]", "<img src=\"/media/mtg/RG.svg\" class=\"mannaSymbol\">");
+        text = text.replaceAll("[wu]", `<img src=\"${rootFolder}WU.svg\" class=\"mannaSymbol\">`);
+        text = text.replaceAll("[uw]", `<img src=\"${rootFolder}WU.svg\" class=\"mannaSymbol\">`);
+        text = text.replaceAll("[wb]", `<img src=\"${rootFolder}WB.svg\" class=\"mannaSymbol\">`);
+        text = text.replaceAll("[bw]", `<img src=\"${rootFolder}WB.svg\" class=\"mannaSymbol\">`);
+        text = text.replaceAll("[wr]", `<img src=\"${rootFolder}RW.svg\" class=\"mannaSymbol\">`);
+        text = text.replaceAll("[rw]", `<img src=\"${rootFolder}RW.svg\" class=\"mannaSymbol\">`);
+        text = text.replaceAll("[wg]", `<img src=\"${rootFolder}GW.svg\" class=\"mannaSymbol\">`);
+        text = text.replaceAll("[gw]", `<img src=\"${rootFolder}GW.svg\" class=\"mannaSymbol\">`);
+        text = text.replaceAll("[ub]", `<img src=\"${rootFolder}UB.svg\" class=\"mannaSymbol\">`);
+        text = text.replaceAll("[bu]", `<img src=\"${rootFolder}UB.svg\" class=\"mannaSymbol\">`);
+        text = text.replaceAll("[ur]", `<img src=\"${rootFolder}UR.svg\" class=\"mannaSymbol\">`);
+        text = text.replaceAll("[ru]", `<img src=\"${rootFolder}UR.svg\" class=\"mannaSymbol\">`);
+        text = text.replaceAll("[ug]", `<img src=\"${rootFolder}GU.svg\" class=\"mannaSymbol\">`);
+        text = text.replaceAll("[gu]", `<img src=\"${rootFolder}GU.svg\" class=\"mannaSymbol\">`);
+        text = text.replaceAll("[br]", `<img src=\"${rootFolder}BR.svg\" class=\"mannaSymbol\">`);
+        text = text.replaceAll("[rb]", `<img src=\"${rootFolder}BR.svg\" class=\"mannaSymbol\">`);
+        text = text.replaceAll("[bg]", `<img src=\"${rootFolder}BG.svg\" class=\"mannaSymbol\">`);
+        text = text.replaceAll("[gb]", `<img src=\"${rootFolder}BG.svg\" class=\"mannaSymbol\">`);
+        text = text.replaceAll("[rg]", `<img src=\"${rootFolder}RG.svg\" class=\"mannaSymbol\">`);
+        text = text.replaceAll("[gr]", `<img src=\"${rootFolder}RG.svg\" class=\"mannaSymbol\">`);
 
         // Generic Manna (numbers)
-        text = text.replaceAll("[0]", "<img src=\"/media/mtg/0.svg\" class=\"mannaSymbol\">");
-        text = text.replaceAll("[1]", "<img src=\"/media/mtg/1.svg\" class=\"mannaSymbol\">");
-        text = text.replaceAll("[2]", "<img src=\"/media/mtg/2.svg\" class=\"mannaSymbol\">");
-        text = text.replaceAll("[3]", "<img src=\"/media/mtg/3.svg\" class=\"mannaSymbol\">");
-        text = text.replaceAll("[4]", "<img src=\"/media/mtg/4.svg\" class=\"mannaSymbol\">");
-        text = text.replaceAll("[5]", "<img src=\"/media/mtg/5.svg\" class=\"mannaSymbol\">");
-        text = text.replaceAll("[6]", "<img src=\"/media/mtg/6.svg\" class=\"mannaSymbol\">");
-        text = text.replaceAll("[7]", "<img src=\"/media/mtg/7.svg\" class=\"mannaSymbol\">");
-        text = text.replaceAll("[8]", "<img src=\"/media/mtg/8.svg\" class=\"mannaSymbol\">");
-        text = text.replaceAll("[9]", "<img src=\"/media/mtg/9.svg\" class=\"mannaSymbol\">");
+        text = text.replaceAll("[0]", `<img src=\"${rootFolder}0.svg\" class=\"mannaSymbol\">`);
+        text = text.replaceAll("[1]", `<img src=\"${rootFolder}1.svg\" class=\"mannaSymbol\">`);
+        text = text.replaceAll("[2]", `<img src=\"${rootFolder}2.svg\" class=\"mannaSymbol\">`);
+        text = text.replaceAll("[3]", `<img src=\"${rootFolder}3.svg\" class=\"mannaSymbol\">`);
+        text = text.replaceAll("[4]", `<img src=\"${rootFolder}4.svg\" class=\"mannaSymbol\">`);
+        text = text.replaceAll("[5]", `<img src=\"${rootFolder}5.svg\" class=\"mannaSymbol\">`);
+        text = text.replaceAll("[6]", `<img src=\"${rootFolder}6.svg\" class=\"mannaSymbol\">`);
+        text = text.replaceAll("[7]", `<img src=\"${rootFolder}7.svg\" class=\"mannaSymbol\">`);
+        text = text.replaceAll("[8]", `<img src=\"${rootFolder}8.svg\" class=\"mannaSymbol\">`);
+        text = text.replaceAll("[9]", `<img src=\"${rootFolder}9.svg\" class=\"mannaSymbol\">`);
 
 
         // Update the HTML
